2717158b3f75350907ac970ff0018e3573dff0ef
Jonah-Has <23jhasegawa@athenian.org>
2020-09-16 04:37:01 +0000

    Wrote the program

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,36 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
+        
+        int ret = 0;
+        int raw = Integer.parseInt(binary);
+        int ed = raw;
+        int exp = 0;
+
+        while (ed > 0){
+
+            if (ed % 2 == 0){
+                exp++;
+            }
+            else{
+                ret += (int)(Math.pow(2,exp));
+                exp++;
+            }
+            ed /= 10;
+
+        }
+        return ret;
+
         // REPLACE WITH YOUR CODE HERE
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type in a number in binary format.");
+        String binary = scan.next();
+        System.out.println("That is equal to the decimal value: " + binaryToDecimal(binary));
+        scan.close();
+        
     }
 }
